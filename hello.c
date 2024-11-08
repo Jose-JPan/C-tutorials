5e2b7ec658d2f98dd1a8f295dc832c21d77c75cb
JoseJPan <jose.santos.soares@hotmail.com>
2024-11-08 11:31:21 +0900

    first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+// Hello this is a C file.
+
+#include <stdio.h> // Processor directive to include standard input-output header
+
+int main() {
+    // Code starts here
+    printf("Vai se Foder!\n"); // prints text to the screen
+    return 0; // Exit the program
+    
+
+}
