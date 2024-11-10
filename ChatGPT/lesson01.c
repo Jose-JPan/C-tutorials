ee8a671386185205cf540f69c0fe4587b7c0ac88
JoseJPan <jose.santos.soares@hotmail.com>
2024-11-10 13:33:40 +0900

    A few Modification

---
+++ ChatGPT/lesson01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    float y = 3.14f;
+    char letter = 'A';
+
+    printf("x: %d, y: %f, letter: %c\n", x, y, letter);
+    return 0;
+
+    //OUTPUT: x: 5, y: 3.140000, letter: A
+
+}
